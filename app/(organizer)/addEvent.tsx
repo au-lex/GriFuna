f942ab6e9b9121aba22aafaa565b615ae5e2cc9a
au-lex <aulexc4d2@gmail.com>
2025-07-24 11:10:12 +0100

    oragniser tab layout done

---
+++ app/(organizer)/addEvent.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const addEvent = () => {
+  return (
+    <View>
+      <Text>addEvent</Text>
+    </View>
+  )
+}
+
+export default addEvent
+
+const styles = StyleSheet.create({})
---
+++ app/(organizer)/findTalent.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const findTalent = () => {
+  return (
+    <View>
+      <Text>findTalent</Text>
+    </View>
+  )
+}
+
+export default findTalent
+
+const styles = StyleSheet.create({})
